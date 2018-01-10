cc08e6de365451bcdeb9c9f4fd0023a5465c6e74
Logan Sparks <logan.sparks1999@gmail.com>
2018-01-10 11:27:32 -0500

    Frozen Logurt

---
+++ Frozenlogurt.py
@@ -0,0 +1,19 @@
+#This is a comment. It will not run while the program runs.
+
+
+print('Sometimes you want to get input from a user.')
+
+
+print('Luckily, there is a function for that!')
+
+
+print('So, what's your name?')
+
+
+name = input()
+
+
+print('So, your name is ' +name+'?')
+
+
+prubt('Sounds weird.')
